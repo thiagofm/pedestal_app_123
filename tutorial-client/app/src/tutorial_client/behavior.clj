8edf504944b4f2d0f39ec8cbe3e7f6353b41eea8
Thiago Fernandes Massa <thiagown@gmail.com>
2013-07-09 16:38:10 -0300

    Making a counter -- I couldn't see that the value under :my-counter is one... where is it? in data ui page?

--- tutorial-client/app/src/tutorial_client/behavior.clj
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -2,10 +2,16 @@
     (:require [clojure.string :as string]
               [io.pedestal.app.messages :as msg]))
 
-(defn set-value-transform [old-value message]
-  (:value message))
+;(defn set-value-transform [old-value message]
+  ;(:value message))
+
+;(def example-app
+  ;{:version 2
+   ;:transform [[:set-value [:greeting] set-value-transform]]})
 
 (def example-app
   {:version 2
-   :transform [[:set-value [:greeting] set-value-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]})
 
+(defn inc-transform [old-value _]
+  ((fnil inc 0) old-value))
